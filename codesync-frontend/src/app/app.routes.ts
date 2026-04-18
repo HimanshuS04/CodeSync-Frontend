c9e81e2fe9eadca5ec8f3dd6494908ba3bbdd109
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air-3.local>
2026-04-19 03:05:16 +0530

    Himanshu.Add UC1: Auth Frontend - Login, Register, JWT Interceptor, Guards

---
+++ codesync-frontend/src/app/app.routes.ts
@@ -0,0 +1,23 @@
+import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'auth/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('././modules/auth/auth.routes')
+        .then(m => m.authRoutes)
+  },
+  {
+    path: 'dashboard',
+    canActivate: [authGuard],
+    loadChildren: () =>
+      import('././modules/dashboard/dashboard.routes')
+        .then(m => m.dashboardRoutes)
+  }
+];
