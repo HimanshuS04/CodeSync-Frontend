7567ba0f80f1d18e7336b405397230ec0b07db30
Himanshu Singh <himanshusingh@Himanshus-MacBook-Air-3.local>
2026-04-22 17:01:52 +0530

    Himanshu.add execution service that support multiple language execution

---
+++ codesync-frontend/src/app/core/services/execution.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+export interface RunCodeRequest {
+  projectId: string;
+  fileId: string;
+  language: string;
+  sourceCode: string;
+  stdin?: string;
+}
+
+export interface ExecutionResult {
+  id: number;
+  language: string;
+  status: string;
+  stdout?: string;
+  stderr?: string;
+  compileOutput?: string;
+  executionTimeMs?: number;
+  memoryUsedKb?: number;
+  createdAt: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExecutionService {
+  private baseUrl = environment.apiGatewayUrl;
+
+  constructor(private http: HttpClient) {}
+
+  runCode(data: RunCodeRequest): Observable<ExecutionResult> {
+    return this.http.post<ExecutionResult>(
+      `${this.baseUrl}/api/executions/run`, data);
+  }
+}
